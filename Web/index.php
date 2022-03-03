13815308e6eef1d13c1971e84d91958168f0ae02
WillyArdiano <willyjessiel99@gmail.com>
2022-03-02 22:05:55 -0600

    Agregando imagenes a utilizar/mejorando indentacion en index.php

--- Web/index.php
+++ Web/index.php
@@ -33,22 +33,22 @@ $cholquij = $nahual." ". strval($energia);
 <body>
 
 <?php include "NavBar.php" ?>
- <div>
- <section id="inicio">
-    <div id="inicioContainer" class="inicio-container">
-      <h1><br><br>Bienvenido al Tiempo Maya</h1>
-      <div id='formulario' style="padding: 15px; width: auto;">
-      <h5 style="color: whitesmoke;">Calendario Haab : <?php echo isset($haab) ? $haab : ''; ?></h5>
-      <h5 style="color: whitesmoke;">Calendario Cholquij : <?php echo isset($cholquij) ? $cholquij : ''; ?></h5>
-      <h5 style="color: whitesmoke;">Cuenta Larga : <?php echo isset($cuenta_larga) ? $cuenta_larga : ''; ?></h5>
-      <label style="color: whitesmoke;"><?php echo isset($fecha_consultar) ? $fecha_consultar : ''; ?></label>
-</div>
-    </div>
-  </section>
- </div>
+  <div>
+    <section id="inicio">
+      <div id="inicioContainer" class="inicio-container">
+        <h1><br><br>Bienvenido al Tiempo Maya</h1>
+        <div id='formulario' style="padding: 15px; width: auto;">
+          <h5 style="color: whitesmoke;">Calendario Haab : <?php echo isset($haab) ? $haab : ''; ?></h5>
+          <h5 style="color: whitesmoke;">Calendario Cholquij : <?php echo isset($cholquij) ? $cholquij : ''; ?></h5>
+          <h5 style="color: whitesmoke;">Cuenta Larga : <?php echo isset($cuenta_larga) ? $cuenta_larga : ''; ?></h5>
+          <label style="color: whitesmoke;"><?php echo isset($fecha_consultar) ? $fecha_consultar : ''; ?></label>
+        </div>
+      </div>
+    </section>
+  </div>
  
   
-  <?php include "blocks/bloquesJs1.html" ?>
+<?php include "blocks/bloquesJs1.html" ?>
 
 </body>
 </html>
