6a840c17a28db1ed4dee193776a52eff762ebb45
WillyArdiano <willyjessiel99@gmail.com>
2022-03-04 23:44:02 -0600

    Empezando implementación de gráficos en el index / agregando link de referencia de los gráficos a la información

--- Web/index.php
+++ Web/index.php
@@ -13,6 +13,10 @@ $energia = include 'backend/buscar/conseguir_energia_numero.php';
 $haab = include 'backend/buscar/conseguir_uinal_nombre.php';
 $cuenta_larga = include 'backend/buscar/conseguir_fecha_cuenta_larga.php';
 $cholquij = $nahual." ". strval($energia);
+$nombre_nahual = $nahual;
+$numero_energia = strval($energia);
+$fecha_haab = $haab[0];
+$nombre_uinal = $haab[1];
 
 ?>
 <!DOCTYPE html>
@@ -38,7 +42,17 @@ $cholquij = $nahual." ". strval($energia);
       <div id="inicioContainer" class="inicio-container">
         <h1><br><br>Bienvenido al Tiempo Maya</h1>
         <div id='formulario' style="padding: 15px; width: auto;">
-          <h5 style="color: whitesmoke;">Calendario Haab : <?php echo isset($haab) ? $haab : ''; ?></h5>
+          <?php 
+            echo "<a href='models/paginaModeloElemento.php?elemento=uinal#".$nombre_uinal."'>";
+            echo "<img src=\"img/Uinales/".$nombre_uinal.".png\" alt=\"Error al intentar mostrar al uinal ".$nombre_uinal." \" width=\"100px\" height=\"100px\" >";
+            echo "</a>";
+          ?>
+          <h5 style="color: whitesmoke;">Calendario Haab : <?php echo isset($fecha_haab) ? $fecha_haab : ''; ?></h5>
+          <?php 
+            echo "<a href='models/paginaModeloElemento.php?elemento=nahual#".$nombre_nahual."'>";
+            echo "<img src=\"img/Nahuales/".$nombre_nahual.".png\" alt=\"Error al intentar mostrar al nahual ".$nombre_nahual." \" width=\"100px\" height=\"100px\" >";
+            echo "</a>";  
+          ?>          
           <h5 style="color: whitesmoke;">Calendario Cholquij : <?php echo isset($cholquij) ? $cholquij : ''; ?></h5>
           <h5 style="color: whitesmoke;">Cuenta Larga : <?php echo isset($cuenta_larga) ? $cuenta_larga : ''; ?></h5>
           <label style="color: whitesmoke;"><?php echo isset($fecha_consultar) ? $fecha_consultar : ''; ?></label>
